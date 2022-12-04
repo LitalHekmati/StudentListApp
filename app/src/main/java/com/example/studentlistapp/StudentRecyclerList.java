bcee884d908dd8bca27a8cbe97f0570057066d42
lital.hekmati <litalhechmati@gmail.com>
2022-12-04 23:45:06 +0200

    design the application

--- app/src/main/java/com/example/studentlistapp/StudentRecyclerList.java
+++ app/src/main/java/com/example/studentlistapp/StudentRecyclerList.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
